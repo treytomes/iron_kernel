1c47ff35a115679c0912637978f3ad15979dce05
Trey Tomes <trey.tomes@gmail.com>
2026-02-23 16:12:16 -0600

    feat: synchronous file access when needed

--- Userland/Extensions/FileSystemTextExtensions.cs
+++ Userland/Extensions/FileSystemTextExtensions.cs
@@ -24,4 +24,38 @@ public static class FileSystemTextExtensions
 		var data = Encoding.UTF8.GetBytes(text);
 		return fs.WriteAsync(url, data, "text/plain", ct);
 	}
+
+	// -----------------------------
+	// Synchronous ReadText
+	// -----------------------------
+	public static string ReadText(
+		this IFileSystem fs,
+		string url,
+		CancellationToken ct = default)
+	{
+		// Force synchronous completion
+		var result = fs.ReadAsync(url, ct)
+			.ConfigureAwait(false)
+			.GetAwaiter()
+			.GetResult();
+
+		return Encoding.UTF8.GetString(result.Data);
+	}
+
+	// -----------------------------
+	// Synchronous WriteText
+	// -----------------------------
+	public static FileWriteResult WriteText(
+		this IFileSystem fs,
+		string url,
+		string text,
+		CancellationToken ct = default)
+	{
+		var data = Encoding.UTF8.GetBytes(text);
+
+		return fs.WriteAsync(url, data, "text/plain", ct)
+			.ConfigureAwait(false)
+			.GetAwaiter()
+			.GetResult();
+	}
 }
